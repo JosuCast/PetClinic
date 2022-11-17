dac995cbc4470241167ce72c0013abd3e36ba722
Aldaaairr <aldaair10@gmail.com>
2022-11-17 13:56:43 -0500

    Fix project
    Change-Id: I5c2a49d418275ad4b5fca770e355354d4fee9123

--- src/main/java/com/tecsup/petclinic/dto/PetDTO.java
+++ src/main/java/com/tecsup/petclinic/dto/PetDTO.java
@@ -1,60 +1,40 @@
-package com.tecsup.petclinic.entities;
+package com.tecsup.petclinic.dto;
 
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 /**
  * 
  * @author jgomezm
  *
  */
-@Entity(name = "pets")
-public class Pet {
+public class PetDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	private String name;
-	@Column(name = "type_id")
-	private int typeId;
-	@Column(name = "owner_id")
-	private int ownerId;
 	
-	@Column(name = "birth_date")
+	private String name;
+	
+	private int typeId;
+
+	private int ownerId;
+
 	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@JsonFormat(pattern = "yyyy-MM-dd",  timezone = "GMT+8")
 	private Date birthDate;
+
+	public PetDTO(String name, int typeId, int ownerId, Date birthDate) {
+		super();
+		this.name = name;
+		this.typeId = typeId;
+		this.ownerId = ownerId;
+		this.birthDate = birthDate;
+	}
+
 	
-	public Pet() {
-	}
-
-	public Pet(long id, String name, int type_id, int owner_id, Date birth_date) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.typeId = type_id;
-		this.ownerId = owner_id;
-		this.birthDate = birth_date;
-	}
-
-	public Pet(String name, int type_id, int owner_id, Date birth_date) {
-		super();
-		this.name = name;
-		this.typeId = type_id;
-		this.ownerId = owner_id;
-		this.birthDate = birth_date;
-
-	}
-
 	public long getId() {
 		return id;
 	}
@@ -87,12 +67,6 @@ public class Pet {
 		this.ownerId = owner_id;
 	}
 
-	@Override
-	public String toString() {
-		return "Pet [id=" + id + ", name=" + name + ", typeId=" + typeId + ", ownerId=" + ownerId + ", birthDate="
-				+ birthDate + "]";
-	}
-
 	public Date getBirthDate() {
 		return birthDate;
 	}
@@ -101,6 +75,10 @@ public class Pet {
 		this.birthDate = birthDate;
 	}
 
-
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", name=" + name + ", typeId=" + typeId + ", ownerId=" + ownerId + ", birth_date="
+				+ birthDate + "]";
+	}
 
 }
